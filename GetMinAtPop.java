832c0c931eee330b367a92932f30a70aeb5a0deb
Raj Balhara <42884781+coder9a@users.noreply.github.com>
2021-05-27 23:14:32 +0530

    Add files via upload

---
+++ GetMinAtPop.java
@@ -0,0 +1,87 @@
+/**
+ * Now, we'll try to solve a famous stack problem.
+ * You are given an array A of size N.
+ * You need to first push the elements of the array into a stack and
+ * then print minimum in the stack at each pop.
+ *
+ * Example 1:
+ *
+ * Input:
+ * N = 5
+ * A = {1 2 3 4 5}
+ * Output:
+ * 1 1 1 1 1
+ * Explanation:
+ * After pushing elements to the stack,
+ * the stack will be "top -> 5, 4, 3, 2, 1".
+ * Now, start popping elements from the stack
+ * popping 5: min in the stack is 1.popped 5
+ * popping 4: min in the stack is 1. popped 4
+ * popping 3: min in the stack is 1. popped 3
+ * popping 2: min in the stack is 1. popped 2
+ * popping 1: min in the stack is 1. popped 1
+ * Example 2:
+ *
+ * Input:
+ * N = 7
+ * A = {1 6 43 1 2 0 5}
+ * Output:
+ * 0 0 1 1 1 1 1
+ * Explanation:
+ * After pushing the elements to the stack,
+ * the stack will be 5->0->2->1->43->6->1.
+ * Now, poping the elements from the stack:
+ * popping 5: min in the stack is 0. popped 5
+ * popping 0: min in the stack is 0. popped 0
+ * popping 2: min in the stack is 1. popped 2
+ * popping 1: min in the stack is 1. popped 1
+ * popping 43: min in the stack is 1.
+ *             popped 43
+ * popping 6: min in the stack is 1. popped 6
+ * popping 1: min in the stack is 1. popped 1.
+ */
+
+import java.util.Stack;
+
+public class GetMinAtPop
+{
+    static Stack<Integer> stack = new Stack<>();;
+    public static void push(int arr[], int n)
+    {
+        stack.push(arr[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(stack.peek()>arr[i])
+            {
+                stack.push(arr[i]);
+            }
+            else
+            {
+                stack.push(stack.peek());
+            }
+        }
+    }
+    public static void pop()
+    {
+        while(!stack.isEmpty())
+        {
+            int x = stack.pop();
+            System.out.println("Popped Element "+x);
+        }
+    }
+    public static void display()
+    {
+        if(stack.isEmpty())
+            System.out.println("Stack Underflow");
+        else
+            System.out.println("Stack elements "+stack);
+    }
+    public static void main(String[] args)
+    {
+        int arr [] = {1,2,3,4,5};
+        push(arr, arr.length);
+        display();
+        pop();
+
+    }
+}
